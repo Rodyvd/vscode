293ab9a63befca20df0c17aba935f94e0ee14712
Rodyvd <114142546+Rodyvd@users.noreply.github.com>
2025-03-06 14:10:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Mohamad!'
+
+    pod_name = os.getenv('MY_POD_NAME', 'Unknown Pod')
+    
+    return f"Hello, Mohamad! You are on pod {pod_name}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
