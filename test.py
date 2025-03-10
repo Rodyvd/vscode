fe342b6a556c954823b35606e2806d11803942bd
Rodyvd <114142546+Rodyvd@users.noreply.github.com>
2025-03-10 13:05:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,21 @@
-Onderzoekend vermogen: informatie verkrijgen en informatie toepassen	Het vermogen in professionele situaties vast te stellen welke kans of welk probleem zicht voordoet en welke kennis nodig is om dit aan te pakken, zo nodig gemis aan kennis op gestructureerde en methodische wijze aan te vullen, zich eigen te maken en uiteindelijk toe te passen op de professionele situatie, of beroepscontext.  
+Investigative Ability: Obtaining and Applying Information
+The ability to determine in professional situations which opportunity or problem is emerging and what knowledge is needed to tackle it, and if necessary, to supplement any lack of knowledge in a structured and methodical way, to acquire it, and ultimately to apply it to the professional situation or occupational context.
 
-Toelichting: 
+Explanation:
 
+Organizational Ability: Planning, Managing, and Justifying the Approach
+The ability to identify, within the given context, factors that can influence (the work on) an assignment; to create a plan by organizing (sub)tasks over time with the available resources, taking into account ethical and cultural aspects and with a focus on sustainability; and to demonstrate the ability to monitor progress and, if necessary, take appropriate action in response to unexpected or conflicting events and to justify these actions to stakeholders.
 
+Explanation:
 
+Interactive Ability: Collaborating and Communicating
+The ability to actively collaborate with relevant partners by contributing to a common outcome with the input of one’s own qualities, always keeping the interests of all (internal/external) parties involved in mind. This also involves communicating appropriately to all target groups to achieve the desired impact.
 
-Organiserend vermogen:
-plannen en managen en
-verantwoorden van de werkwijze
-	Het vermogen om binnen de gegeven context factoren vast te stellen, die (het werken aan) een opdracht kunnen beïnvloeden, hiervoor een planning op te stellen door (deel)taken te ordenen in de tijd met de beschikbare middelen, rekening houdend met ethische en culturele aspecten, mede gericht op duurzaamheid. Daarbij het vermogen tonen om de voortgang te bewaken en zo nodig passende actie te ondernemen bij onverwachte of conflicterende gebeurtenissen en deze te kunnen verantwoorden naar de stakeholders.  
+Explanation:
 
-Toelichting:
+Learning Ability: Being Proactive/Showing Leadership, Reflecting
+The ability to take control of one’s own (learning) process, to determine how it can contribute to personal development and a future in ICT, and to take responsibility for it.
 
+Explanation:
 
-
-
-Interactief vermogen:
-Samenwerken en communiceren	Het vermogen actief samen te werken met relevante partners door bij te dragen aan een gezamenlijk resultaat met de inbreng van eigen kwaliteiten en altijd gericht op de belangen van alle (intern/extern) betrokkenen. Daarbij passend naar alle doelgroepen communiceren om de gewenste impact te bereiken.
-
-Toelichting: 
-
-
-
-
-Lerend vermogen:
-proactief zijn/leiderschap tonen
-reflecteren	Het vermogen de regie over het eigen (leer)proces te nemen, te bepalen hoe dit kan bijdragen aan de persoonlijke ontwikkeling en een toekomst in de ICT, en daarvoor zelf de verantwoordelijkheid te nemen.
-
-Toelichting:
-
-
-
-
-
-Ontwikkeladvies:
-
-
-
-
-
-
-
-
+Development Advice:
