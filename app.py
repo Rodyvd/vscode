3a8953bdc47169f369d037e1694a45cf37d98ffb
Rodyvd <114142546+Rodyvd@users.noreply.github.com>
2025-03-10 11:27:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
 
     pod_name = os.getenv('MY_POD_NAME', 'Unknown Pod')
     
-    return f"Hello, Rody! You are on pod {pod_name}"
+    return f"Hello, Rody!! You are on pod {pod_name}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
