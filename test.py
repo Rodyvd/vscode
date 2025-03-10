48af885692b2445434bc6f5eb2d6cb24d9d4509d
Rodyvd <114142546+Rodyvd@users.noreply.github.com>
2025-03-10 13:03:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,44 @@
-import jwt
+Onderzoekend vermogen: informatie verkrijgen en informatie toepassen	Het vermogen in professionele situaties vast te stellen welke kans of welk probleem zicht voordoet en welke kennis nodig is om dit aan te pakken, zo nodig gemis aan kennis op gestructureerde en methodische wijze aan te vullen, zich eigen te maken en uiteindelijk toe te passen op de professionele situatie, of beroepscontext.  
 
-def decode_jwt_insecure(token):
-    # Insecure: Disabling signature verification
-    decoded = jwt.decode(token, verify=False)  # Noncompliant
-    print("Decoded token (insecure):", decoded)
+Toelichting: 
 
-def decode_jwt_insecure_with_options(token, key):
-    # Insecure: Disabling signature verification using options
-    decoded = jwt.decode(token, key, options={"verify_signature": False})  # Noncompliant
-    print("Decoded token with options (insecure):", decoded)
 
-def main():
-    token = "your.jwt.token.here"
-    key = "your-secret-key"
 
-    decode_jwt_insecure(token)
-    decode_jwt_insecure_with_options(token, key)
 
-if __name__ == "__main__":
-    main()
+Organiserend vermogen:
+plannen en managen en
+verantwoorden van de werkwijze
+	Het vermogen om binnen de gegeven context factoren vast te stellen, die (het werken aan) een opdracht kunnen beïnvloeden, hiervoor een planning op te stellen door (deel)taken te ordenen in de tijd met de beschikbare middelen, rekening houdend met ethische en culturele aspecten, mede gericht op duurzaamheid. Daarbij het vermogen tonen om de voortgang te bewaken en zo nodig passende actie te ondernemen bij onverwachte of conflicterende gebeurtenissen en deze te kunnen verantwoorden naar de stakeholders.  
+
+Toelichting:
+
+
+
+
+Interactief vermogen:
+Samenwerken en communiceren	Het vermogen actief samen te werken met relevante partners door bij te dragen aan een gezamenlijk resultaat met de inbreng van eigen kwaliteiten en altijd gericht op de belangen van alle (intern/extern) betrokkenen. Daarbij passend naar alle doelgroepen communiceren om de gewenste impact te bereiken.
+
+Toelichting: 
+
+
+
+
+Lerend vermogen:
+proactief zijn/leiderschap tonen
+reflecteren	Het vermogen de regie over het eigen (leer)proces te nemen, te bepalen hoe dit kan bijdragen aan de persoonlijke ontwikkeling en een toekomst in de ICT, en daarvoor zelf de verantwoordelijkheid te nemen.
+
+Toelichting:
+
+
+
+
+
+Ontwikkeladvies:
+
+
+
+
+
+
+
+
