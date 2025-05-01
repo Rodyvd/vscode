afdfac5c0514ccf5c8950328002260e1427061fc
Rodyvd <114142546+Rodyvd@users.noreply.github.com>
2025-05-01 09:54:25 +0200

    Create providers.tf

---
+++ terraform/vm-defectdojo/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "2b24783d-69a5-4d13-a614-ea8fe9a35b8d"
+}
